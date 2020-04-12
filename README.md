137153f33fb689eaa22b9b4b3a98203c172172da
richirrim <ricardoortega.dev@gmail.com>
2020-04-12 17:52:46 -0500

    Clon landing del EDCamp

---
+++ README.md
@@ -0,0 +1,21 @@
+# Proyecto final del curso de RWD EDTeam
+
+**⚒️ Nombre del proyecto:** Landign page del EDCamp
+
+**📝 Descripción:**
+
+En este curso como proyecto final el reto fue crear una landign page. El profesor Álvaro Felipe nos enseñó como clonar la landing page del EDCamp en su versión mobile y desktop.  Usamos la metodología "mobile first" para el response web design.
+
+En la creación de esta landing se implementaron cosas como custom propertys para organizar la paleta de colores del sitio, para la tipografia, variables de apilamiento (z-index), breakpoint, etc.
+
+Se utilizó SASS para tener un proyecto más escalable y con super powers, una variación de la arquitectura SMACSS para organizar el código.
+
+Como **plus** yo cree el HTML usando PUGJS y para automatizar ciertas tareas implemente GULP4.
+
+Y por último utilicé Browser-Sync para refrescar el sitio cada vez que gulp detectara un cambio en archivos HTML, CSS Y JS.
+
+
+
+- 🔗 Plataforma: https://ed.team/
+- 🔗 Curso: [Responsive Web Design](https://ed.team/cursos/rwd)
+- 🔗 Enlace a mi certificado: https://ed.team/richirrim/curso/rwd
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const { series, parallel, dest, src, watch } = require('gulp')
+const imagemin = require('gulp-imagemin')
+const babel = require('gulp-babel')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+
+sass.compiler = require('node-sass')
+
+var paths = {
+    raiz: 'public',
+    html: {
+      src: 'dev/**/*.pug',
+      dest: 'public/'
+    },
+    styles: {
+      src: 'dev/scss/**/*.scss',
+      dest: 'public/css/'
+    },
+    scripts: {
+      src: 'dev/js/**/*.js',
+      dest: 'public/js/'
+    },
+    images: {
+      src: 'dev/images/**/*',
+      dest: 'public/images/'
+    }
+};
+
+function server() {
+  browserSync.init({
+      server: {
+          baseDir: paths.raiz
+      }
+  });
+}
+
+function babelJs () {
+  return src(paths.scripts.src)
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(dest(paths.scripts.dest))
+}
+
+function pugToHtml () {
+    return src(paths.html.src)
+      .pipe(pug({pretty: true}))
+      .pipe(dest(paths.html.dest))
+}
+
+function sassToCss () {
+  return src(paths.styles.src)
+    .pipe(sass())
+      .on('error', sass.logError)
+    .pipe(dest(paths.styles.dest))
+}
+
+function optimizeImg () {
+  return src(paths.images.src)
+    .pipe(imagemin())
+    .pipe(dest(paths.images.dest))
+}
+
+function watchFiles () {
+  watch(paths.scripts.src, babelJs)
+  watch(paths.html.src, pugToHtml)
+  watch(paths.styles.src, sassToCss)
+  watch(paths.html.dest).on('change', browserSync.reload)
+  watch(paths.styles.dest).on('change', browserSync.reload);
+}
+
+exports.watch = watchFiles
+exports.default = series(optimizeImg, parallel(server, watchFiles))
